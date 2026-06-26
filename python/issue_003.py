95245565e040d22bf706ae12f05710b150b6a843
Lluc Nicolau <34487003+llucnf@users.noreply.github.com>
2026-06-26 11:18:04 +0000

    complete Issue #3:

---
+++ python/issue_003.py
@@ -0,0 +1,9 @@
+user_num=int(input("Enter a number: "))
+if user_num < 13:
+    print("Child")
+elif user_num < 18 :
+    print("Teenager")
+elif user_num <= 64 :
+        print("Adult")
+else :
+    print("Senior")
