2145113ce1984d17cfb16e937f5a2e245b00b485
Lluc Nicolau <34487003+llucnf@users.noreply.github.com>
2026-07-14 07:24:27 +0000

    feat: implement issue #4 - first function

---
+++ python/issue_004.py
@@ -0,0 +1,10 @@
+# estaria mejor esta version?
+user_name = input("Enter your name: ")
+
+def greet_user(name):
+    print("Hello " +name )
+    
+    print("Welcome to Programming Lab!")
+
+
+greet_user(user_name)
